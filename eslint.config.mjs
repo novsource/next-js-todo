30f50826f88e4a6de7895daf3c4e964eb12709d1
Poli Sour <polisour.work@gmail.com>
2026-04-23 18:01:44 +0800

    chore(app): init app

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+  },
+
+  // Parse the `.gitignore` file to get the ignores, on by default
+  gitignore: true,
+
+  typescript: true,
+  react: true,
+})
